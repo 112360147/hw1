e5c55220d25b9a250688f36604ec44851af11a4d
112360147 <161786277+112360147@users.noreply.github.com>
2024-03-07 20:32:56 +0800

    shapes.h

--- shapes.h
+++ shapes.h
@@ -32,5 +32,21 @@ public:
 		return (width * height);
 	}
 };
+class Triangle : public Shape{
+public:
+	Triangle(double a = 0, double b = 0) :Shape(a, b) { }
 
+	double area(){
+		return(width * height * 0.5);
+	}
+};
+
+class Circle : public Shape{
+public:
+	Circle(double a = 0, double b = 0) :Shape(a, b) { }
+
+	double area(){
+		return(width * width * PI );
+	}
+};
 #endif
